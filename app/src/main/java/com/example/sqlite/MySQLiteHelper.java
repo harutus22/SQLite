512dd7c8445f3b6f3fee0e46311a9fcc12d41c9b
harutus22 <harutus22@yahoo.com>
2019-08-31 16:13:26 +0400

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/MySQLiteHelper.java
@@ -0,0 +1,31 @@
+package com.example.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "database";
+    public static final String TABLE_NAME = "IKEA";
+    public static final String ID = "ID";
+    public static final String NAME = "NAME";
+    public static final String SURNAME = "SURNAME";
+    public static final String MARKS = "MARKS";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME +
+                "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " + "NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
